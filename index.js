252d4fa51f06a0338dd010c5413241da6c6bafd3
Ashok15041996 <ashokbaskaranasp@gmail.com>
2024-08-09 23:37:27 +0530

    GuviTask-4

---
+++ index.js
@@ -0,0 +1,94 @@
+// 1) Resume in JSON Format
+// let myResume={
+//     "basics": {
+//     "name": "Ashokkumar B",
+//     "email": "ashokbaskaranas@gamil.com",
+//     "phone": 6380621014,
+//     "degree": "B,Sc (Agriculture)",
+//     "location": {
+//     "address": "4/147 , Aknimariyamman Temple Street",
+//     "postalCode": 635702,
+//     "city": "Thirupathur",
+//     "state": "Tamilnadu",
+//     "country": "India"
+//     },
+//     },
+//     "work": [
+//     {
+//     "company": "Department of Agriculture",
+//     "position": "District Co-ordinator",
+//     "summary": "I am Working in PKVY Scheme. create more Organic Farmers ",
+//     },
+//     ],
+//     "education": [
+//     {
+//     "institution": "Annamalai University",
+//     "department": "Agriculture",
+//     "studyType": "fulltime",
+//     "batch start year": 2013,
+//     "batch end year": 2017,
+//     "gpa": 7.8,
+//     }
+//     ],
+//     "skills": [
+//     {
+//     "name": "javascript",
+//     "level": "beginer",
+//     }
+//     ],
+//     "languages": [
+//     {
+//     "language": "Tamil,Enlish",
+//     }
+//     ],
+//     "interests": [
+//     {
+//     "name": "To Learn More Computer Works",
+//     }
+//     ]
+//     }
+//     console.log(myResume);
+
+
+
+// 2)JSON iterate overall for loops (for, for In, for Of, for Each)
+    // var json = [{
+    //     "id" : "Ashok101996", 
+    //     "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    //     "task" : "zen portal task",
+    //     "mail": "ashok101996@gmail.com"
+    // },
+    // {
+    //     "id" : "Ashok199610", 
+    //     "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    //     "task" : "zen portal task",
+    //     "mail": "ashok199610@gmail.com"
+    // }];
+    // //for loop
+    // for(var i = 0; i < json.length; i++) {
+    //     var obj = json[i];
+    
+    //     console.log(obj.id);
+    //     console.log(obj.msg);
+    //     console.log(obj.task);
+    //     console.log(obj.mail);
+    
+    // }
+    // //for Each
+    // json.forEach(function(obj) { console.log(obj.msg); });
+    
+    // //for In
+    // for (var key in json) {
+    // if (json.hasOwnProperty(key)) {
+    //   console.log(json[key].id);
+    //   //console.log(json[key].msg);
+    
+    // }
+    // }
+    // //for Of
+    // let text = "";
+    // for (let x of json[key].id) {
+    //  text += x; 
+    // }
+    //  console.log(text)
+    
